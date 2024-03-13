039d8ae07941361fe012eccaeb3829bb68b2da30
Gregory Baker <gregory.baker@duke.edu>
2024-03-13 14:13:30 -0400

    Basic Python Lambda (IE-18)
    Motivation
    ---
    Create a template project for creating new lambda functions running Python.
    
    Modifications
    ---
    - Added dockerfile
    - Added function handler
    - Added readme instructions

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# ie2-py-lamda-template
+# Project Description
 A Python lambda template for use with Insight Engine 2.0
+
+# Pre-Requisites
+    - Docker Desktop
+
+# Run Locally
+
+## WINDOWS
+
+1. Build the Container
+    - `docker run --platform linux/amd64 -p 9000:8080 ie2pylambdatemplate:latest`
+2. Test your Function
+    - `Invoke-WebRequest -Uri "http://localhost:9000/2015-03-31/functions/function/invocations" -Method Post -Body '{}' -ContentType "application/json"`
+
+## OSX / LINUX
+
+1. Build the Container
+    - `docker run --platform linux/amd64 -p 9000:8080 ie2pylambdatemplate:latest`
+2. Test your Function
+    - `curl "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{}'`
---
+++ lambda_function.py
@@ -0,0 +1,25 @@
+import os
+import logging
+
+ENV_LOG_LEVEL       = "IE2-LOGGING-LEVEL"
+ENV_ENV             = "IE2-ENV"
+ENV_DEFAULT         = ""
+LOG_LEVEL_DEFAULT   = "INFO"
+ENV_DEFAULT_ENV     = "DEV"
+
+# setup logger
+logger = logging.getLogger()
+loglvl = os.getenv(ENV_LOG_LEVEL, LOG_LEVEL_DEFAULT)
+
+logger.setLevel(loglvl)
+
+def handler(ev, ctx):
+    res = "Handler finished!"
+    logger.info('## ENVIRONMENT VARIABLES')
+    logger.info(os.getenv('AWS_LAMBDA_LOG_GROUP_NAME', ENV_DEFAULT))
+    logger.info(os.getenv('AWS_LAMBDA_LOG_STREAM_NAME', ENV_DEFAULT))
+    logger.info(os.getenv(ENV_LOG_LEVEL, LOG_LEVEL_DEFAULT))
+    logger.info('## EVENT')
+    logger.info(ev)
+    logger.info(res)
+    return res
