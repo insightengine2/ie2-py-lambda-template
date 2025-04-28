03ad2e5be8ae01a1e72e8052c8119dc486c2c7a4
Gregory Baker <gregory.baker@duke.edu>
2025-04-28 15:32:08 -0400

    Update Build (IE-155)
    Motivation
    ---
    The python template needed its build scripts updated, including the ie2-config.yml.
    
    Modifications
    ---
    - Updated build scripts
    - Updated ie2-config

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,75 @@
 #!/bin/bash
+cd ..
 
-declare output="./dist/${CODEBUILD_WEBHOOK_TRIGGER}/${CODEBUILD_BUILD_NUMBER}"
+BUILDDIR="build"
+DEPSDIR="deps"
+FILENAME="bootstrap.zip"
+REQSFILENAME="requirements.txt"
+DISTDIR="dist"
+RUNTIMEARCH="manylinux2014_aarch64"
+PYTHONVER="3.13.3"
 
-echo ${output}
+# remove remnants of previous builds
+if [ -d $BUILDDIR ]; then
+    echo "$BUILDDIR exists!"
+    echo "Preparing to delete $BUILDIR"
+    rm -r $BUILDDIR/
+fi
 
-mkdir ../build
+if [ -d $DISTDIR ]; then
+    echo "$DISTDIR exists!"
+    echo "Preparing to delete $DISTDIR"
+    rm -r $DISTDIR/
+fi
 
+mkdir $DISTDIR
+
+# copy source code to build directory
+cp -r ./src ./$BUILDDIR
+
+cd $BUILDDIR
+
+# add source files to zip archive
+zip -r "../$DISTDIR/$FILENAME" "."
+
+# prepare a deps directory
+mkdir ./$DEPSDIR
+
+# install requirements generator tool
+pip install pipreqs
+
+# generate requirements.txt
+pipreqs . --force --ignore=tests
+
+# check if requirements file is empty first
+if [ -e $REQSFILENAME ] && [ ! -s $REQSFILENAME ]; then
+    echo "$REQSFILENAME exists AND is empty..."
+    echo "Skipping dependencies install."
+else
+    # install dependencies to deps directory
+    pip install --platform $RUNTIMEARCH --python-version $PYTHONVER --only-binary=:all: --target ./$DEPSDIR -r $REQSFILENAME
+
+    # check if any dependencies were downloaded
+    # find "./$DEPSDIR/" -maxdepth 0 -empty | read v
+    filecnt=$(ls -A "./$DEPSDIR" | wc -l)
+    echo "Found $filecnt files!"
+    if [ $filecnt -gt 0 ]; then
+        echo "Dependencies found. Updating archive..."
+        cd $DEPSDIR
+        zip -ur "../../$DISTDIR/$FILENAME" "."
+        cd ..
+    else
+        echo "No dependencies found in $DEPSDIR."
+    fi
+fi
+
+echo "Build Complete"
+
+cd ../$DISTDIR
+
+if [ -f "./$FILENAME" ]; then
+    filesize=$(wc --bytes "./$FILENAME")
+    echo "Wrote $filesize"
+else 
+    echo "...but unable to find $FILENAME!"
+fi
